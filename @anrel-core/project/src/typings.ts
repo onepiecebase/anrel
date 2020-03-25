509f3984c2a5c878834712ac71636fe14eb23b40
zhongjiahao <zhongjiahao@yy.com>
2020-03-25 20:15:39 +0800

    style: 优化代码

--- @anrel-core/project/src/typings.ts
+++ @anrel-core/project/src/typings.ts
@@ -1,3 +1,11 @@
-export interface Demo {
+export interface ProjectInterface {
   [key: string]: any;
+  version: string;
+  packagesScope: string[];
+  repoURI: string[];
+  load: (cwd?: string) => Promise<void>;
+  /** 根配置文件路径 */
+  rootConfigLoc: string;
+  /** 项目根目录路径 */
+  rootPath: string;
 }
