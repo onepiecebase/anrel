e21f54f539884ba3c1139d41d823a96cfb02a1c9
zhongjiahao <zhongjiahao@yy.com>
2020-03-26 01:50:14 +0800

    build: 基础建设

--- @anrel-plugin/git/src/typings.ts
+++ @anrel-plugin/git/src/typings.ts
@@ -1,3 +1,3 @@
 export interface Demo {
-  [key: string]: any;
+  [key: string]: any
 }
---
+++ lint-staged.config.js
@@ -0,0 +1,65 @@
+const fs = require('fs-extra')
+const path = require('path')
+const glob = require('glob')
+const flattenDeep = require('lodash/flattenDeep')
+const fromPairs = require('lodash/fromPairs')
+
+async function findScopePaths() {
+  const rootPath = process.cwd()
+  const json = await fs.readJSON(path.join(rootPath, 'lerna.json'))
+  const promises = json.packages.map(pattern => {
+    return new Promise((resolve, reject) => {
+      glob(pattern, { cwd: rootPath }, (error, matches) => {
+        error ? reject(error) : resolve(matches)
+      })
+    })
+  })
+
+  const files = await Promise.all(promises).then((...files) => flattenDeep(files))
+  const folders = files.filter(file => fs.statSync(file).isDirectory() && fs.existsSync(path.join(file, 'package.json')))
+  const packages = folders.map(folder => [path.join(rootPath, folder), folder])
+  return fromPairs(packages)
+}
+
+function runPrefix(callback) {
+  return async filenames => {
+    const scopePaths = await findScopePaths()
+    const folders = Object.keys(scopePaths)
+
+    const scopes = {}
+    filenames.forEach(filename => {
+      const scope = folders.find(folder => filename.search(folder) === 0)
+      if (!Array.isArray(scopes[scope])) {
+        scopes[scope] = []
+      }
+
+      scopes[scope].push(filename)
+    })
+
+    const commands = Object.keys(scopes).map(name => {
+      const packages = scopes[name]
+      const prefix = scopePaths[name]
+      return callback(prefix, packages)
+    })
+
+    commands.push(`git add ${filenames.join(' ')}`)
+    return commands.join(' && ')
+  }
+}
+
+function run(command) {
+  return runPrefix((prefix, packages) => {
+    return `npm run ${command} ${prefix ? `--prefix ${prefix}` : ''} ${packages.join(' ')}`
+  })
+}
+
+module.exports = {
+  '**/*.{md,json,yml}': async filenames => {
+    const format = run('lint-stage:format')
+    return await format(filenames)
+  },
+  '**/*.{ts,tsx,d.ts}': async filenames => {
+    const lint = run('lint-stage:format')
+    return await lint(filenames)
+  },
+}
