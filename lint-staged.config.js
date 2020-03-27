daf251673425bfa055c14d034442bf75240ee1ee
zhongjiahao <zhongjiahao@yy.com>
2020-03-27 13:20:17 +0800

    test: test

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -4,7 +4,9 @@ const glob = require('glob')
 const flattenDeep = require('lodash/flattenDeep')
 const fromPairs = require('lodash/fromPairs')
 
-async function findScopePaths() {
+const scopeCache = {}
+
+const findScopePaths = async () => {
   const rootPath = process.cwd()
   const json = await fs.readJSON(path.join(rootPath, 'lerna.json'))
   const promises = json.packages.map(pattern => {
@@ -21,36 +23,71 @@ async function findScopePaths() {
   return fromPairs(packages)
 }
 
-function runPrefix(callback) {
-  return async filenames => {
-    const scopePaths = await findScopePaths()
-    const folders = Object.keys(scopePaths)
+const matchScopes = async (filenames, cache) => {
+  if (cache && scopeCache[cache]) {
+    return scopeCache[cache]
+  }
 
-    const scopes = {}
-    filenames.forEach(filename => {
-      const scope = folders.find(folder => filename.search(folder) === 0)
+  const scopePaths = await findScopePaths()
+  const folders = Object.keys(scopePaths)
+
+  const scopes = {}
+  const discharge = []
+
+  filenames.forEach(filename => {
+    const scope = folders.find(folder => filename.search(folder) === 0)
+    if (scope) {
       if (!Array.isArray(scopes[scope])) {
         scopes[scope] = []
       }
 
       scopes[scope].push(filename)
-    })
+    } else {
+      discharge.push(filename)
+    }
+  })
 
+  const data = { scopes, discharge }
+  if (cache) {
+    scopeCache[cache] = data
+  }
+
+  return data
+}
+
+const runEachScope = (command) => {
+  return async filenames => {
+    const { scopes } = await matchScopes(filenames)
     const commands = Object.keys(scopes).map(name => {
       const packages = scopes[name]
       const prefix = scopePaths[name]
-      return callback(prefix, packages)
+      return `npm run ${command} ${prefix ? `--prefix ${prefix}` : ''} ${packages.join(' ')}`
     })
 
     return commands.join(' && ')
   }
 }
 
-function run(command) {
-  return runPrefix((prefix, packages) => {
-    return `npm run ${command} ${prefix ? `--prefix ${prefix}` : ''} ${packages.join(' ')}`
-  })
+const runDischarge = (command) => {
+  return async filenames => {
+    const { discharge } = await matchScopes(filenames)
+    return `npm run ${command} ${discharge.join(' ')}`
+  }
 }
+
+const run = (command) => {
+  return async (filenames) => {
+    const scopeCommand = await runEachScope(command)(filenames)
+    const dischargeCommand = await runDischarge(command)(filenames)
+    return [scopeCommand, dischargeCommand].filter(Boolean).join(' && ')
+  }
+}
+
+~(async filenames => {
+  const format = await run('lint-stage:format')
+  const command = format(filenames)
+  return [command, `git add ${filenames.join(' ')}`]
+})(['/Users/zhongjiahao/Develop/utils/anrel/scripts/lerna/command/add.ts'])
 
 module.exports = {
   '**/*.{md,json,yml}': async filenames => {
@@ -59,8 +96,8 @@ module.exports = {
     return [command, `git add ${filenames.join(' ')}`]
   },
   '**/*.{ts,tsx,d.ts}': async filenames => {
-    const lint = run('lint-stage:lint:ts')
-    const command = await lint(filenames)
+    const format = run('lint-stage:lint:ts')
+    const command = await format(filenames)
     return [command, `git add ${filenames.join(' ')}`]
   },
 }
