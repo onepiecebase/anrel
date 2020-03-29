fa2ec9a39a32efee7ba3525f14b3369a511d6ed8
zhongjiahao <zhongjiahao@yy.com>
2020-03-30 01:33:14 +0800

    build: remove scripts

--- scripts/lerna/constants/conf.ts
+++
@@ -1,3 +0,0 @@
-import path from 'path'
-
-export const rootPath = path.join(__dirname, '../../../')
--- scripts/lerna/index.ts
+++
@@ -1,9 +0,0 @@
-import { program } from 'commander'
-
-import './command/add'
-import './command/exec'
-
-program
-  .name('lerna-script')
-  .usage('<command> [options]')
-  .parse(['node', 'lerna-script'].concat(process.argv.slice(2)))
